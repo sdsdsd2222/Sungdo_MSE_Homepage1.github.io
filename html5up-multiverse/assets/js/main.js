36d860fa5c4f0df4d023447afddcc4a1a88fefda
eatingchicken <117004075+eatingchicken@users.noreply.github.com>
2022-12-10 19:16:34 +0900

    Update main.js
    Signed-off-by: eatingchicken <117004075+eatingchicken@users.noreply.github.com>

--- html5up-multiverse/assets/js/main.js
+++ html5up-multiverse/assets/js/main.js
@@ -279,3 +279,25 @@
 				});
 
 })(jQuery);
+//교가 나오게 해주는 코드
+var audio = document.getElementById("video");
+$(function(){
+	$('.play').on('click', function(e){
+		e = e || window.event;
+		var btn = e.target;
+		if (!audio.paused){
+			//일시정지
+			audio.pause();
+			isPlaying = false;
+		} else {
+			//재생
+			audio.play();
+			isPlaying = true;
+		}
+	});
+	$('.stop').on('click', function(e){
+		audio.pause();
+		audio.currentTime = 0;
+		isPlaying = false;
+	});
+});
